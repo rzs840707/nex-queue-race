c28974f5348111dd02df10b204ab79a82e3d2909
audeSt <audestick@gmail.com>
2016-12-03 17:13:52 +0800

    update

--- session-api/src/main/java/core/SessionFilter.java
+++ session-api/src/main/java/core/SessionFilter.java
@@ -1,15 +1,19 @@
 package core;
 
+import me.hao0.common.util.Strings;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import javax.servlet.*;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.util.Map;
 
 /**
  * Created by lrkin on 2016/12/2.
  */
-public class SessionFilter implements Filter {
+public abstract class SessionFilter implements Filter {
     private final static Logger log = LoggerFactory.getLogger(SessionFilter.class);
     protected final static String SESSION_COOKIE_NAME = "sessionCookieName";
     protected final static String DEFAULT_SESSION_COOKIE_NAME = "sfid";
@@ -97,12 +101,52 @@ public class SessionFilter implements Filter {
      */
     protected abstract SessionManager createSessionManager() throws IOException;
 
-    private void initAttrs(FilterConfig filterConfig) {
+    private void initAttrs(FilterConfig config) {
+        String param = config.getInitParameter(SESSION_COOKIE_NAME);
+        sessionCookieName = Strings.isNullOrEmpty(param) ? DEFAULT_COOKIE_CONTEXT_PATH : param;
+        param = config.getInitParameter(MAX_INACTIVE_INTERVAL);
+        maxInactiveInterval = Strings.isNullOrEmpty(param) ? DEFAULT_MAX_INACTIVE_INTERVAL : Integer.parseInt(param);
+        cookieDomain = config.getInitParameter(COOKIE_DOMAIN);
+        param = config.getInitParameter(COOKIE_CONTEXT_PATH);
+        cookieContextPath = Strings.isNullOrEmpty(param) ? DEFAULT_COOKIE_CONTEXT_PATH : param;
+        param = config.getInitParameter(COOKIE_MAX_AGE);
+        cookieMaxAge = Strings.isNullOrEmpty(param) ? DEFAULT_COOKIE_MAX_AGE : Integer.parseInt(param);
+        log.info("SessionFilter (sessionCookieName={},maxInactiveInterval={},cookieDomain={})",
+                sessionCookieName, maxInactiveInterval, cookieDomain);
     }
 
     @Override
-    public void doFilter(ServletRequest servletRequest, ServletResponse servletResponse, FilterChain filterChain) throws IOException, ServletException {
+    public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain) throws IOException, ServletException {
+        if (request instanceof HttpServletRequestWrapper2) {
+            chain.doFilter(request, response);
+            return;
+        }
+        HttpServletRequest httpRequest = (HttpServletRequest) request;
+        HttpServletResponse httpResponse = (HttpServletResponse) response;
+        HttpServletRequestWrapper2 request2 = new HttpServletRequestWrapper2(httpRequest, httpResponse, sessionManager);
+        request2.setSessionCookieName(sessionCookieName);
+        request2.setMaxInactiveInterval(maxInactiveInterval);
+        request2.setCookieDomain(cookieDomain);
+        request2.setCookieContextPath(cookieContextPath);
+        request2.setCookieMaxAge(cookieMaxAge);
 
+        //do other filter
+        chain.doFilter(request2, response);
+
+        HttpSessionWrapper session = request2.currentSession();
+        if (session != null) {
+            if (!session.isValid()) {
+                //if invalidate , delete session
+                log.debug("session is invalid, will be deleted");
+                WebUtil.failureCookie(httpRequest, httpResponse, sessionCookieName, cookieDomain, cookieContextPath);
+            } else if (session.isDirty()) {
+                //should flush to store
+                log.debug("try to flush session to session store");
+                Map<String, Object> snapshot = session.snapshot();
+                
+
+            }
+        }
     }
 
     @Override
